dabfe4c508a31bb9211d1cb1971386028e865346
Raven Chen <raven1996@foxmail.com>
2017-08-30 13:07:52 +0800

    remove alpha to reduce buffer size

--- src/panorama.js
+++ src/panorama.js
@@ -112,7 +112,7 @@ function Panorama(viewerId, img) {
 		gl.activeTexture(gl.TEXTURE0);
 		var textureObject = gl.createTexture();
 		gl.bindTexture(gl.TEXTURE_2D, textureObject);
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, imgObj);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, imgObj);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
